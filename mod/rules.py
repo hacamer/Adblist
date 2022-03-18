1a8d63a4ae23c456aa3a106abb5b039f592aa1a1
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-03-18 22:06:50 +0800

    Create rules.py

---
+++ mod/rules.py
@@ -0,0 +1,13 @@
+# -*- coding:gbk -*-
+import os
+
+result = []
+ffo = open("adp.txt")
+fo = open("adp-test.txt", "w")
+result=list(set(ffo.readlines()))
+result.sort()
+fo.writelines(result)
+fo.close()
+ffo.close()
+os.remove("adp.txt")
+os.rename("adp-test.txt","abp.txt")
